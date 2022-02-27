8f7af70a6e67c05e84bf4615586fc3c89a35781a
Chembampul <Khatenevich@bk.ru>
2022-02-27 23:35:24 +0300

    Initial commit

---
+++ src/main/java/com/example/lab1_cpp/Lab1CppApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1_cpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab1CppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1CppApplication.class, args);
+    }
+
+}
