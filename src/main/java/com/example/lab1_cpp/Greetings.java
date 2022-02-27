8f1bbf56e84dd362a42cb1eb24b22efb8fadddd2
Chembampul <Khatenevich@bk.ru>
2022-02-27 23:41:19 +0300

    Added Lab №1

---
+++ src/main/java/com/example/lab1_cpp/Greetings.java
@@ -0,0 +1,16 @@
+package com.example.lab1_cpp;
+
+public class Greetings {
+
+
+    private int fibonachi;
+
+    public Greetings(int value_get) {
+        this.fibonachi = value_get;
+    }
+
+    public int getValue() {
+        return fibonachi;
+    }
+
+}
